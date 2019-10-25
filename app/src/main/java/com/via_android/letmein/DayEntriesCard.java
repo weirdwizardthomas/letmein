208f80ecbb3abecf3a55da57b9b776a69db66c8d
weirdwizardtom <koristka.tom@gmail.com>
2019-10-25 12:46:31 +0200

    Added an activity for day entries

---
+++ app/src/main/java/com/via_android/letmein/DayEntriesCard.java
@@ -0,0 +1,14 @@
+package com.via_android.letmein;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DayEntriesCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_entries_card);
+    }
+}
