3107d74ffc7948cc86d67ad590a4cad60cf86a58
weirdwizardtom <koristka.tom@gmail.com>
2019-10-25 12:47:55 +0200

    Added a recycler view of visitors in a day, that will be nested in a recycler view of individual days

--- app/src/main/java/com/via_android/letmein/MainActivity.java
+++ app/src/main/java/com/via_android/letmein/MainActivity.java
@@ -1,14 +1,55 @@
 package com.via_android.letmein;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.via_android.letmein.Adapters.MemberAdapter;
+import com.via_android.letmein.Entities.Member;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView membersList;
+    RecyclerView.Adapter membersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        membersList = findViewById(R.id.members_list);
+        membersList.hasFixedSize();
+        membersList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        List<Member> members = new ArrayList<>();
+
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+        members.add(new Member("a", "b", R.mipmap.profile_icon_placeholder));
+
+        membersAdapter = new MemberAdapter(members);
+        membersList.setAdapter(membersAdapter);
     }
 }
