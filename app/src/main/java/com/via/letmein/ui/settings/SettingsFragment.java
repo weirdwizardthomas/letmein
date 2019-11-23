147476196dda50184f6fcdf6bffe53cdd08cb02a
weirdwizardtom <koristo1@fit.cvut.cz>
2019-11-23 16:37:50 +0100

    Added Settings with pin code change

--- app/src/main/java/com/via/letmein/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/via/letmein/ui/settings/SettingsFragment.java
@@ -1,28 +1,31 @@
 package com.via.letmein.ui.settings;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
+import com.amirarcane.lockscreen.activity.EnterPinActivity;
 import com.via.letmein.R;
 
-
-public class SettingsFragment extends Fragment {
-
-    public SettingsFragment() {
-    }
-
+public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_settings, container, false);
-        return root;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        Preference preference = preferenceScreen.findPreference("reset_pin");
+        if (preference != null)
+            preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = EnterPinActivity.getIntent(getContext(), true);
+                    startActivity(intent);
+                    return true;
+                }
+            });
+
     }
-
-
 }
