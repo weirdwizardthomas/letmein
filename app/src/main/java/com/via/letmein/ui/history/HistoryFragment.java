12d6078ecd9aa2124c1079e865ea3b5989c9e23d
weirdwizardtom <koristo1@fit.cvut.cz>
2019-12-01 18:01:05 +0100

    Added default values to date range

--- app/src/main/java/com/via/letmein/ui/history/HistoryFragment.java
+++ app/src/main/java/com/via/letmein/ui/history/HistoryFragment.java
@@ -22,16 +22,18 @@ import com.via.letmein.R;
 import com.via.letmein.persistence.room.entity.DayEntry;
 import com.via.letmein.ui.history.day_entry.DayEntryAdapter;
 
-import java.util.Date;
 import java.util.List;
 
 public class HistoryFragment extends Fragment {
+
+    public static final int WEEK_IN_MILISECONDS = 604800000;
 
     private RecyclerView dayEntryRecyclerView;
     private RecyclerView.Adapter dayEntryAdapter;
     private HistoryViewModel historyViewModel;
     private Button openCalendarButton;
     private MaterialDatePicker<Pair<Long, Long>> picker;
+    private Pair<Long, Long> selectionDates;
 
 
     @Nullable
@@ -43,34 +45,40 @@ public class HistoryFragment extends Fragment {
 
         initialiseAdapter();
         initialiseDaysRecyclerView(root);
+        setDefaultSelectionDates();
         initialiseDatePicker();
         initialiseDatePickerButton(root);
+
+        picker.show(getChildFragmentManager(), picker.toString());
 
         return root;
     }
 
+    private void setDefaultSelectionDates() {
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+        long weekAgo = today - WEEK_IN_MILISECONDS;
+        selectionDates = new Pair<>(weekAgo, today);
+    }
+
     private void initialiseDatePickerButton(View root) {
         openCalendarButton = root.findViewById(R.id.openCalendarButton);
+        openCalendarButton.setText("Select range to show:");
         openCalendarButton.setOnClickListener(v -> picker.show(getChildFragmentManager(), picker.toString()));
     }
 
     private void initialiseDatePicker() {
         picker = setupDateSelectorBuilder().build();
         picker.addOnPositiveButtonClickListener(selection -> {
-            Pair<Date, Date> dateRange = new Pair<>(new Date(selection.first), new Date(selection.second));
-            openCalendarButton.setText(picker.getHeaderText());
+            selectionDates = selection;
+            openCalendarButton.setText("Select range to show:" + picker.getHeaderText());
         });
     }
 
     private MaterialDatePicker.Builder<Pair<Long, Long>> setupDateSelectorBuilder() {
-        long today = MaterialDatePicker.todayInUtcMilliseconds();
-        Pair<Long, Long> todayPair = new Pair<>(today, today);
-
-        final MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker
-                .Builder
-                .dateRangePicker();
-        builder.setSelection(todayPair);
-        builder.setTheme(resolveOrThrow(getContext(), R.attr.materialCalendarTheme));
+        final MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder
+                .dateRangePicker()
+                .setSelection(selectionDates)
+                .setTheme(resolveOrThrow(getContext(), R.attr.materialCalendarTheme));
         return builder;
     }
 
