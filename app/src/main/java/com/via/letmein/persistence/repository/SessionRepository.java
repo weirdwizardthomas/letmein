f3e7436eaa8dd004bde90ec2b63beb2009036129
weirdwizardtom <koristo1@fit.cvut.cz>
2019-12-03 09:57:16 +0100

    Changed access to error constants from private to public

--- app/src/main/java/com/via/letmein/persistence/repository/SessionRepository.java
+++ app/src/main/java/com/via/letmein/persistence/repository/SessionRepository.java
@@ -19,14 +19,14 @@ import retrofit2.Response;
 
 public class SessionRepository {
 
-    private static final String ERROR_MISSING_REQUIRED_PARAMETERS = "missing_request_parameters";
-    private static final String ERROR_SHORT_USERNAME = "short_username_length";
-    private static final String ERROR_DATABASE_ERROR = "database_error";
-    private static final String ERROR_ADMIN_ALREADY_EXISTS = "admin_already_exists";
-    private static final String ERROR_WRONG_SERIAL_ID = "wrong_serial_id";
-    private static final String ERROR_NAME_ALREADY_IN_USE = "name_already_in_use";
-    private static final String ERROR_MEMBER_IS_ALREADY_AN_ADMIN = "member_is_already_an_admin";
-    private static final String ERROR_WRONG_USER_PASSWORD = "wrong_user_password";
+    public static final String ERROR_MISSING_REQUIRED_PARAMETERS = "missing_request_parameters";
+    public static final String ERROR_SHORT_USERNAME = "short_username_length";
+    public static final String ERROR_DATABASE_ERROR = "database_error";
+    public static final String ERROR_ADMIN_ALREADY_EXISTS = "admin_already_exists";
+    public static final String ERROR_WRONG_SERIAL_ID = "wrong_serial_id";
+    public static final String ERROR_NAME_ALREADY_IN_USE = "name_already_in_use";
+    public static final String ERROR_MEMBER_IS_ALREADY_AN_ADMIN = "member_is_already_an_admin";
+    public static final String ERROR_WRONG_USER_PASSWORD = "wrong_user_password";
 
     private static SessionRepository instance;
     private Api api;
